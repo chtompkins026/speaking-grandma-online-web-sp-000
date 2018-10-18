cba13c0146ccbf4f944281d34383709d170de4c4
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-18 19:26:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  if phrase == ""
+  if phrase == "I LOVE YOU GRANDMA!"
+    return 
   elsif phrase == phrase.upcase 
     return "NO, NOT SINCE 1938!" 
   
